369544793664bf073f10c2b98103553c78e2b289
CollinsKegode <collinskegode@gmail.com>
2022-12-20 21:55:28 +0300

    added appjs

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-console.log('my first nodejs web application');
-console.log('added a new line of code');
-console.log('added a new file');
+import express from 'express'
+
+const app = express()
+
+//routes
+//homepage
+app.get('/', (req, res) => {
+    res.send('home page')
+})
+
+//about
+app.get('/about', (req, res) => {
+    res.send('about page')
+})
+
+//signup
+app.get('/signup', (req, res) => {
+    res.send('you can register here.')
+})
+
+//404 error
+app.get('*', (req, res) => {
+    res.send('404 \n Page not found')
+})
+
+app.listen(3000)
